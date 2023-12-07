5932c7055326950cfdf52b3843e777ccec1de3c5
AxelCarrillo <122809603+AxelCarrillo@users.noreply.github.com>
2023-12-07 09:46:22 -0600

    CREATED

---
+++ sql/usuarios.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuarios(
+    username varchar,
+    password varchar,
+    token varchar,
+    timestamp varchar
+);
+
+INSERT INTO usuarios (username, password, token, timestamp) VALUES ('oppie@gmail.com', 'a88730092144187f8d7b4d940456154a', '1234', DateTime('now')) ;
